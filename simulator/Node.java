44e8c5bfb46efa48f0981e29723346a9514369fb
JosephTheCoder <menesesjmmc@gmail.com>
2018-05-02 15:26:50 +0100

    node

--- simulator/Node.java
+++ simulator/Node.java
@@ -1,5 +1,25 @@
 package simulator;
 
 public class Node {
+	private int edges[4];
+	private int type;
+	
+	
+	public Node(int[] edges, int node_type) 
+	{
+		this.type = node_type;
 
+		System.arraycopy(edges, 0, this.edges, 0, edges.length);
+	}
+	
+	public int[] getEdges()
+	{
+		return this.edges;
+	}
+	
+	public int getType()
+	{
+		return this.type;
+	}
+	
 }
