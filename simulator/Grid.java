43ea67ab032898d4370844457cfdad8a635eea1a
JosephTheCoder <menesesjmmc@gmail.com>
2018-05-02 18:30:15 +0100

    grid node addition

--- simulator/Grid.java
+++ simulator/Grid.java
@@ -1,8 +1,33 @@
 package simulator;
 
+import java.util.*;
+
+import jdk.internal.jimage.ImageReader.Node;
+
 public class Grid {
 	int width;
 	int height;
+	List<Node> lines[];
+	
+	/*Lista de Nodes*/
+	/*Lista de listas de Nodes -> linhas*/
+	
+	
+	public Grid(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		
+		lines = new List<Node>[height];;
+	}
+	
+	public void addNode(int[] position, int type, int[] edges)
+	{
+		Node newNode = new Node(position, edges, type);
+	
+		lines[position[1]].add(newNode);
+		
+	}
 	
 	
 
