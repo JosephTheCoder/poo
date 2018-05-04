16cdb2c5e4d2b509fc71e5fae6418128a5c545cb
JosephTheCoder <menesesjmmc@gmail.com>
2018-05-04 23:07:29 +0100

    Map and XML functional

--- src/simulator/Grid.java
+++ src/simulator/Grid.java
@@ -1,7 +1,5 @@
 package simulator;
 
-import java.util.*;
-
 /**
  * Class for the Map main representation
  * 
@@ -16,23 +14,49 @@ public class Grid {
 	private int height;
 	private Node map[][];
 	
+	private static final int empty = 0;
+	private static final int obstacle = 1;
 	
-	public Grid(int width, int height)
+	public Grid(int width, int height, int[][] obstacles, int[][] specialZones)
 	{
+		int[] position = new int[2];
+		
 		this.width = width;
 		this.height = height;
 		
-		map = new Node[width][height];
+		map = new Node[height][width];
+		
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				position[0] = j;
+				position[1] = i;
+				
+				addNode(position, empty);
+			}
+		}
+		
+		setObstacles(obstacles);
+		setSpecialZones(specialZones);
 	}
 	
-	public void addNode(int[] position, int type, int[] edges)
+	
+	/*
+	 * 
+	 */
+	public void addNode(int[] position, int type)
 	{
-		Node newNode = new Node(position, edges, type);
+		Node newNode = new Node(position, type);
 	
 		map[position[1]][position[0]] = newNode;
 		
 	}
 	
+	
+	/*
+	 * 
+	 */
 	public Node getNode(int[] position)
 	{
 		return map[position[1]][position[0]];
@@ -40,13 +64,85 @@ public class Grid {
 	
 	
 	/*
-	 * Method that returns the next adjacent available node to a given position
+	 * int[][] -> [xpos,ypos]
 	 */
-	public Node getNextAvailableNode(int[] position)
+	private void setObstacles(int[][] obstacles)
 	{
-		
+		for(int i = 0; i < obstacles.length; i++) {
+			System.out.println(obstacles[i][0] + " " + obstacles[i][1]);
+			System.out.println(getNode(obstacles[i]).toString());
+			getNode(obstacles[i]).setType(obstacle);
+		}
 	}
 	
+	
+	/*
+	 * 
+	 */
+	private void setSpecialZones(int[][] specialZones)
+	{
+		int xstart, ystart, xend, yend, weight, x, y;
+		int[] position = new int[2];
+		
+		for(int i = 0; i < specialZones.length; i++) {
+			xstart = specialZones[i][0];
+			ystart = specialZones[i][1];
+			xend = specialZones[i][2];
+			yend = specialZones[i][3];
+			weight = specialZones[i][4];
+			
+			for(y = ystart; y <= yend; y++)
+			{
+				for(x = xstart; x <= xend; x++)
+				{
+					position[0] = x;
+					position[1] = y;
+					
+					if(x < xend)
+						getNode(position).setRightEdge(weight);
+					
+					if(x > xstart)
+						getNode(position).setLeftEdge(weight);
+					
+					if(y > ystart)
+						getNode(position).setUpperEdge(weight);
+					
+					if(y < yend)
+						getNode(position).setBottomEdge(weight);
+				}	
+			}
+		}
+	}
+	
+	
+	/*
+	 * 
+	 */
+	public boolean isEmptyPosition(int[] position)
+	{
+		if(map[position[1]][position[0]].getType() == empty)
+			return true;
+		else
+			return false;
+	}
+	
+	
+	/*
+	 * 
+	 */
+	public int[] getNextPosition(int[] actualPosition)
+	{
+		int[] position = new int[2];
+		
+		//TODO faz o random a volta
+		
+		return position;
+	}
+	
+	
+	/*
+	 * Returns a string with the given line
+	 */
 	public String toString(int line)
 	{
 		String str = new String();
@@ -57,9 +153,13 @@ public class Grid {
 		return str;
 	}
 	
+	/*
+	 * Prints the map
+	 */
 	public void printGrid()
 	{
-		for(int i = 0; i < height; i++)
+		System.out.println("\nMap:");
+		for(int i = height - 1; i >= 0; i--)
 			System.out.println(toString(i));
 	}
 }
--- src/simulator/Node.java
+++ src/simulator/Node.java
@@ -1,19 +1,21 @@
 package simulator;
 
 public class Node {
-	private int position[] = new int[2]; /*[x, y]*/
+	private int[] position = new int[2]; /*[x, y]*/
 	
-	private int edges[] = new int[4]; /*-1 for obstacles, all other int for weight*/
-										/*[up, down, left, right]*/
+	private int[] edges = new int[4]; /* weight		[up, down, left, right]*/
 	private int type;
 	
 	
-	public Node(int[] position, int[] edges, int node_type) 
+	public Node(int[] position, int node_type) 
 	{
 		this.type = node_type;
 		
 		System.arraycopy(position, 0, this.position, 0, position.length);
-		System.arraycopy(edges, 0, this.edges, 0, edges.length);
+		edges[0] = 1;
+		edges[1] = 1;
+		edges[2] = 1;
+		edges[3] = 1;
 	}
 	
 	public int[] getPosition()
@@ -31,16 +33,44 @@ public class Node {
 		return this.type;
 	}
 	
+	public void setUpperEdge(int value)
+	{
+		if(edges[0] < value)
+			edges[0] = value;
+	}
+	
+	public void setBottomEdge(int value)
+	{
+		if(edges[1] < value)
+			edges[1] = value;
+	}
+
+	public void setLeftEdge(int value)
+	{
+		if(edges[2] < value)
+			edges[2] = value;
+	}
+	
+	public void setRightEdge(int value)
+	{
+		if(edges[3] < value)
+			edges[3] = value;
+	}
+	
+	public void setType(int type)
+	{
+		this.type = type;
+	}
+	
 	public String toString()
 	{
-		String x = Integer.toString(position[1]);
-		String y = Integer.toString(position[0]);
+		String x = Integer.toString(position[0]);
+		String y = Integer.toString(position[1]);
 		String up = Integer.toString(edges[0]);
 		String down = Integer.toString(edges[1]);
 		String left = Integer.toString(edges[2]);
-		String right = Integer.toString(edges[2]);
+		String right = Integer.toString(edges[3]);
 		
 		return "Node coordinates: [" + x + "," + y + "]  Type: " + type + "  Edges: " + up + " " + down + " " + left + " " + right;
 	}
-	
 }
